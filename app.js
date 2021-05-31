cb79dfcd8b436d22568a3663b2d9f75898c7c340
singh5293 <psingh5293@gmail.com>
2021-05-31 13:13:37 +0530

    updating res

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  res.send("Hello PK");
+  res.send("Hello PK!");
 });
 app.listen(port, () => console.log(`app is running on port number  ${port}`));
